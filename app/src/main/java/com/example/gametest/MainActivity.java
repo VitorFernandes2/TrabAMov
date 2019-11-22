f9409f27eb318ba732e3be989d6f2f51b50b9ab9
Luisilva99 <44380629+Luisilva99@users.noreply.github.com>
2019-11-22 01:09:37 +0000

    Inicio do Projeto

---
+++ app/src/main/java/com/example/gametest/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.gametest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageSwitcher;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageSwitcher imgLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE );
+    }
+}
