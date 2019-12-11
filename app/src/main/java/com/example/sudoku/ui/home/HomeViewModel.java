58ff01acc2eba28453ed3be48324db142d3e4cb2
VitorFernandes2 <vhferprog@gmail.com>
2019-12-11 13:37:55 +0000

    Primeiro menu quase acabado

---
+++ app/src/main/java/com/example/sudoku/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.sudoku.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Sudoku");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
